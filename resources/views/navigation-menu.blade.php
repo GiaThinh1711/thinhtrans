baf1792f1f86241308ec2eb32dc0761c6e19b540
GiaThinh1711 <thinhdau1711@gmail.com>
2021-07-02 00:05:06 +0700

    gg

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -62,9 +62,6 @@
                                         {{ __('Switch Teams') }}
                                     </div>
 
-                                    @foreach (Auth::user()->allTeams() as $team)
-                                        <x-jet-switchable-team :team="$team" />
-                                    @endforeach
                                 </div>
                             </x-slot>
                         </x-jet-dropdown>
