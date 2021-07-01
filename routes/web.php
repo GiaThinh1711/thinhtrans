26ed3e0674ed2957e6447ec107359cb15594f6ce
GiaThinh1711 <thinhdau1711@gmail.com>
2021-07-02 00:02:18 +0700

    gg

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::match(["get", "post"], "login", [LoginController::class, "login"])->name("login");
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('home');
