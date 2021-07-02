4bb6f276e4ae68d933c01918322c27fe7218e20a
GiaThinh1711 <thinhdau1711@gmail.com>
2021-07-02 12:36:38 +0700

    gg

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset("public/dist/img/gg.jpg")}}">
+    <link rel="icon" href="{{asset("public/dist/img/gg.jpg")}}">
     <title>Healthy Food</title>
 
     <!-- Google Font: Source Sans Pro -->
